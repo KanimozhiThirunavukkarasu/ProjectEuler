6efdaf5495334c4c8f4d026da5e63d8fcce859a0
KanimozhiThirunavukkarasu <60142930+KanimozhiThirunavukkarasu@users.noreply.github.com>
2021-12-07 21:43:58 +0530

    Update User.java

--- Day5CaseStudy/User and Main/User.java
+++ Day5CaseStudy/User and Main/User.java
@@ -1,1 +1,67 @@
 
+package Day5;
+
+public class User implements Comparable<User>{
+ private static String name;
+ private String mobileNumber;
+ private String Username;
+ private String password;
+ 
+ User(){
+	 
+ }
+ 
+ public User(String name, String mobileNumber, String username, String password){
+	 this.name=name;
+	 this.mobileNumber=mobileNumber;
+	 this.Username=username;
+	 this.password=password;
+ }
+ 
+ 
+ public static String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getMobileNumber() {
+	return mobileNumber;
+}
+
+public void setMobileNumber(String mobileNumber) {
+	this.mobileNumber = mobileNumber;
+}
+
+public String getUsername() {
+	return Username;
+}
+
+public void setUsername(String username) {
+	Username = username;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+@Override
+ public String toString()
+ {
+	 return "name : \n"+name+"mobileNumber :\n"+mobileNumber+"Username :\n"+Username+"password :\n"+password;
+ }
+
+@Override
+public int compareTo(User user) {
+	return this.name.compareTo(user.name);
+}
+
+
+}
+
